108abd08f3953911257dca4f8d870dc839da6e38
sruthi735644-boop <sruthi735644@gmail.com>
2026-07-03 14:13:44 +0400

    Delete backend directory

--- backend/taskmanager/tasks/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import TaskList,TaskDetail,RegisterView
-
-urlpatterns = [
-    path("tasks/",TaskList.as_view(),name="tasks"),
-    path("tasks/<int:id>/",TaskDetail.as_view(),name="task-detail"),
-    path("register/",RegisterView.as_view(),name='register'),
-]
